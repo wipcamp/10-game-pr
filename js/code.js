f9e43843b4ad0c81763dbe53e41cf19b7b9f2da1
pea <pepea.23@live.com>
2018-02-20 23:27:49 +0700

    a

--- js/code.js
+++ js/code.js
@@ -126,67 +126,41 @@ function createloginfacebook() {
 
 
 
-
-
-	FB.init({
-		/**********************************************************************
-		 * TODO(Developer): Change the value below with your Facebook app ID. *
-		 **********************************************************************/
-		appId: '119399881422143',
-		status: true,
-		xfbml: true,
-		version: 'v2.6'
-	});
-	FB.Event.subscribe('auth.authResponseChange', checkLoginState);
-	function checkLoginState(event) {
-		if (event.authResponse) {
-			// User is signed-in Facebook.
-			var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-				unsubscribe();
-				// Check if we are already signed-in Firebase with the correct user.
-				if (!isUserEqual(event.authResponse, firebaseUser)) {
-					// Build Firebase credential with the Facebook auth token.
-					var credential = firebase.auth.FacebookAuthProvider.credential(
-						event.authResponse.accessToken);
-					// Sign in with the credential from the Facebook user.
-					firebase.auth().signInWithCredential(credential).catch(function (error) {
-						// Handle Errors here.
-						var errorCode = error.code;
-						var errorMessage = error.message;
-						// The email of the user's account used.
-						var email = error.email;
-						// The firebase.auth.AuthCredential type that was used.
-						var credential = error.credential;
-						// ...
-					});
-				} else {
-					game.state.start('CutScene')
-					console.log('aa')
-				}
-			});
-		} else {
-			// User is signed-out of Facebook.
-			firebase.auth().signOut();
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		var token = result.credential.accessToken;
+		// The signed-in user info.
+		var user = result.user;
+		// ...
+	  }).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		// ...
+	  });
+	  firebase.auth().signInWithRedirect(provider);
+	  firebase.auth().getRedirectResult().then(function(result) {
+		if (result.credential) {
+		  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		  var token = result.credential.accessToken;
+		  // ...
 		}
-	}
-	function isUserEqual(facebookAuthResponse, firebaseUser) {
-		if (firebaseUser) {
-			var providerData = firebaseUser.providerData;
-			for (var i = 0; i < providerData.length; i++) {
-				if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
-					providerData[i].uid === facebookAuthResponse.userID) {
-					// We don't need to re-auth the Firebase connection.
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	firebase.auth().signOut().then(function () {
-		// Sign-out successful.
-	}).catch(function (error) {
-		// An error happened.
-	});
+		// The signed-in user info.
+		var user = result.user;
+	  }).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		// ...
+	  });
 }
 function updateloginfacebook() {
 
